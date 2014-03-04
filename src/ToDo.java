805a2e771f71c4a2fcf7b41efba41551a1e589ac
anthonyptetlow <anthonyptetlow@gmai.com>
2014-03-04 12:21:24 +0000

    Updated todo with notes

--- src/ToDo.java
+++ src/ToDo.java
@@ -1,7 +1,13 @@
 public class ToDo {
-	// TODO Fix Poket iplementation.
+	// TODO Fix Poket implementation.
 	// TODO Change all interactions to be saved until the end of an iteration
 	// TODO Rework the Loop systehm such that it con tinues until all balls
 	// stationary
 
+	/*
+	 * Had issues with getting the visual rep to matrch the physics Had 2
+	 * problems with: - Max speed being 300 units per second when 1 unit was a
+	 * mm (should have been 1m) - Rescaled to fix - Elastic/Inelastic threashold
+	 * - edited source code and recompiled
+	 */
 }
