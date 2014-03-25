50bfe279c107bedc1053df571d1dd8eb10d44c90
anthonyptetlow <anthonyptetlow@gmail.com>
2014-03-25 15:48:30 +0000

    Failed to get the AI working

--- src/ai/engine/imp/ReverseShotGenerator.java
+++ src/ai/engine/imp/ReverseShotGenerator.java
@@ -52,32 +52,57 @@ public class ReverseShotGenerator implements IShotGenerator {
 		allBalls.addAll(balls);
 		allBalls.add(w);
 		allBalls.remove(b);
-		if (pathBetweenPointsClear(coordToVec2(p.getPosX(), p.getPosY()), b
-				.getNode().getPosition(), allBalls)) {
+
+		if (pathBetweenPointsClear(coordToVec2(p.getPosX(), p.getPosY()),
+				coordToVec2(b.getPosX(), b.getPosY()), allBalls)) {
 			// TODO Calculate White-Ball Contact Point
-			// double theta = Math.atan((p.getPosY() - b.getPosY())
-			// / (p.getPosX() - b.getPosX()));
-			// double dx = b.getRadius() * Math.cos(theta), dy = b.getRadius()
-			// * Math.cos(theta);
-			//
-			// double xContact = b.getPosX() + dx;
-			// double yContact = b.getPosY() + dy;
-			// Vec2 vecContact = new Vec2((float) xContact, (float) yContact);
-			// TODO Check Angle of attack -90 < x < 90
+			Vec2 contactPoint = calculateWhiteBallCentreForContactPoint(p, b);
 			// check path between this ball and white is clear
-			// TODO change this as the collision point isnt the
-			// centre of the ball
-			if (pathBetweenPointsClear(b.getNode().getPosition(), w.getNode()
-					.getPosition(), balls)) {
-				// TODO change to calculated Shot
+			allBalls.remove(w);
+			allBalls.add(b);
+			if (pathBetweenPointsClear(contactPoint,
+					coordToVec2(w.getPosX(), w.getPosY()), allBalls)) {
+
+				// float perpGrad = perpGradient(
+				// coordToVec2(b.getPosX(), b.getPosY()), contactPoint);
+
 				// If clear add to set of clear paths maybe new
 				// object {Pocket,
 				// PoolBall, WhiteBall, ShotVector(only ind
 				// direction), Option[power]
-				return new Vec2();
+				float xVector = contactPoint.x - w.getPosX();
+				float yVector = contactPoint.y - w.getPosY();
+
+				// TODO Check Angle of attack -90 < x < 90
+				// If angle between perpGrad and shot 0 < x < 180
+				// System.out.println(perpGrad);
+				// double angle1 = Math.atan(perpGrad);
+				// double angle2 = Math.atan(yVector / xVector);
+				// System.out.println("Angle1:" + angle1);
+				// System.out.println("Angle2:" + angle2);
+				// if (0 < (angle2 - angle1) && (angle2 - angle1) < 3.14) {
+				return new Vec2(xVector, yVector);
+				// }
 			}
 		}
 		return null;
+	}
+
+	// private float perpGradient(Vec2 ball, Vec2 contactPoint) {
+	// System.out.println("Ball: " + ball);
+	// System.out.println("ContPoint: " + contactPoint);
+	// return -1.0f / ((ball.y - contactPoint.y) / (ball.x - contactPoint.x));
+	// }
+
+	private Vec2 calculateWhiteBallCentreForContactPoint(IPocket p, IPoolBall b) {
+		double theta = Math.atan((p.getPosY() - b.getPosY())
+				/ (p.getPosX() - b.getPosX()));
+		double dx = b.getRadius() * Math.cos(theta), dy = b.getRadius()
+				* Math.cos(theta);
+
+		double xContact = b.getPosX() + 2 * dx;
+		double yContact = b.getPosY() + 2 * dy;
+		return new Vec2((float) xContact, (float) yContact);
 	}
 
 	private Vec2 coordToVec2(float x, float y) {
@@ -87,14 +112,14 @@ public class ReverseShotGenerator implements IShotGenerator {
 	public boolean pathBetweenPointsClear(Vec2 ob1, Vec2 ob2,
 			List<IPoolBall> balls) {
 		for (IPoolBall ball : balls) {
-			if (!ballNotOnPath(ob1, ob2, ball)) {
+			if (ballOnPath(ob1, ob2, ball)) {
 				return false;
 			}
 		}
 		return true;
 	}
 
-	public boolean ballNotOnPath(Vec2 ob1, Vec2 ob2, IPoolBall ball) {
+	public boolean ballOnPath(Vec2 ob1, Vec2 ob2, IPoolBall ball) {
 		// Math on sheet A for line intersecting a circle
 		float a = ob1.x;
 		float b = ob1.y;
@@ -112,8 +137,20 @@ public class ReverseShotGenerator implements IShotGenerator {
 
 		double o = Math.abs(Math.sin(theta) * h);
 
-		if ((o - ball.getRadius()) > ball.getRadius())
-			return true;
+		if ((o - ball.getRadius()) < ball.getRadius()) {
+			// Calculate n = (a,b) -> (x,y)
+			double n = Math.sqrt(sq(a - x) + sq(b - y));
+			// Calculate m = (c,d) -> (x,y)
+			double m = Math.sqrt(sq(c - x) + sq(d - y));
+			// Calculate p = sqrt(n^2 - o ^2)
+			double p = Math.sqrt(sq(n) - sq(o));
+			// Calculate q = sqrt(m^2 - o ^2)
+			double q = Math.sqrt(sq(m) - sq(o));
+			// if(p + q <= (dist (a,b) -> (c,d) return false
+			if (p + q <= (Math.sqrt(sq(a - c) + sq(b - d)) + (2 * ball
+					.getRadius())))
+				return true;
+		}
 		return false;
 	}
 
--- src/ai/engine/imp/test/ReverseShotGeneratorTest.java
+++ src/ai/engine/imp/test/ReverseShotGeneratorTest.java
@@ -31,46 +31,90 @@ public class ReverseShotGeneratorTest {
 	}
 
 	@Test
+	public void testGetShotIfPossible() {
+		ReverseShotGenerator shotGen = new ReverseShotGenerator();
+		// Straight Shot
+		Vec2 shot = shotGen.getShotIfPossible(new MockPocket(1.0f, 2.0f),
+				new MockPoolBall(2.0f, 1.0f), new MockPoolBall(3.0f, 0.0f),
+				new ArrayList<IPoolBall>());
+		System.out.println(shot);
+		assertTrue(shot != null);
+		//
+		// shot = shotGen.getShotIfPossible(new MockPocket(1.0f, 2.0f),
+		// new MockPoolBall(2.0f, 1.0f), new MockPoolBall(2.0f, 0.0f),
+		// new ArrayList<IPoolBall>());
+		// System.out.println(shot);
+		// assertTrue(shot != null);
+
+		// Vec2 shot = shotGen.getShotIfPossible(new MockPocket(1.0f, 2.0f),
+		// new MockPoolBall(2.0f, 1.0f), new MockPoolBall(1.0f, 1.0f),
+		// new ArrayList<IPoolBall>());
+		// System.out.println(shot);
+		// assertTrue(shot == null);
+
+		//
+		// ArrayList<IPoolBall> balls = new ArrayList<IPoolBall>();
+		// balls.add(new MockPoolBall(1.5f, 1.5f));
+		// shot = shotGen.getShotIfPossible(new MockPocket(1.0f, 2.0f),
+		// new MockPoolBall(2.0f, 1.0f), new MockPoolBall(3.0f, 2.0f),
+		// balls);
+		// assertTrue(shot == null);
+		// balls.clear();
+		//
+		// balls.add(new MockPoolBall(2.51207475f, 1.51267475f));
+		// shot = shotGen.getShotIfPossible(new MockPocket(1.0f, 2.0f),
+		// new MockPoolBall(2.0f, 1.0f), new MockPoolBall(3.0f, 2.0f),
+		// balls);
+		// assertTrue(shot == null);
+		// balls.clear();
+		// TODO check shots on extreme angles
+	}
+
+	@Test
 	public void testBallNotOnPath() {
 		ReverseShotGenerator shotGen = new ReverseShotGenerator();
-		assertTrue("The path is clear", shotGen.ballNotOnPath(new Vec2(1.0f,
-				3.0f), new Vec2(3.0f, 4.0f), new MockPoolBall(3.0f, 3.0f)));
+		assertTrue("The path is clear", shotGen.ballOnPath(
+				new Vec2(1.0f, 3.0f), new Vec2(3.0f, 4.0f), new MockPoolBall(
+						3.0f, 3.0f)));
 
-		assertTrue("The path is clear", shotGen.ballNotOnPath(new Vec2(3.0f,
-				4.0f), new Vec2(1.0f, 3.0f), new MockPoolBall(3.0f, 3.0f)));
+		assertTrue("The path is clear", shotGen.ballOnPath(
+				new Vec2(3.0f, 4.0f), new Vec2(1.0f, 3.0f), new MockPoolBall(
+						3.0f, 3.0f)));
 
 		// Clear Path
-		assertTrue("The path is clear", shotGen.ballNotOnPath(new Vec2(3.0f,
-				1.0f), new Vec2(1.0f, 3.0f), new MockPoolBall(3.0f, 3.0f)));
+		assertTrue("The path is clear", shotGen.ballOnPath(
+				new Vec2(3.0f, 1.0f), new Vec2(1.0f, 3.0f), new MockPoolBall(
+						3.0f, 3.0f)));
 		// not clear Path
-		assertTrue("The path is clear", shotGen.ballNotOnPath(new Vec2(1.0f,
-				3.0f), new Vec2(3.0f, 4.0f), new MockPoolBall(3.0f, 3.0f)));
+		assertTrue("The path is clear", shotGen.ballOnPath(
+				new Vec2(1.0f, 3.0f), new Vec2(3.0f, 4.0f), new MockPoolBall(
+						3.0f, 3.0f)));
 
 		// Just inside
 		assertFalse("The path isn't clear(Just inside Ball)",
-				shotGen.ballNotOnPath(new Vec2(4.0f, 2.950f), new Vec2(1.0f,
+				shotGen.ballOnPath(new Vec2(4.0f, 2.950f), new Vec2(1.0f,
 						2.950f), new MockPoolBall(3.0f, 3.0f)));
 		// On Line
 		assertFalse("The path isn't clear(Just touching Ball)",
-				shotGen.ballNotOnPath(new Vec2(4.0f, 2.946f), new Vec2(1.0f,
+				shotGen.ballOnPath(new Vec2(4.0f, 2.946f), new Vec2(1.0f,
 						2.946f), new MockPoolBall(3.0f, 3.0f)));
 		// just outside
 		assertTrue("The path isn't clear(Just outside Ball)",
-				shotGen.ballNotOnPath(new Vec2(4.0f, 2.940f), new Vec2(1.0f,
+				shotGen.ballOnPath(new Vec2(4.0f, 2.940f), new Vec2(1.0f,
 						2.940f), new MockPoolBall(3.0f, 3.0f)));
 
 		// On an angle
 		// Just inside
 		assertFalse("The path isn't clear(Just inside Ball angled)",
-				shotGen.ballNotOnPath(new Vec2(2.985f, 2.985f), new Vec2(3.0f,
+				shotGen.ballOnPath(new Vec2(2.985f, 2.985f), new Vec2(3.0f,
 						2.0f), new MockPoolBall(3.0f, 3.0f)));
 		// On Line
 		assertFalse("The path isn't clear(Just inside Ball angled)",
-				shotGen.ballNotOnPath(new Vec2(2.980f, 2.980f), new Vec2(3.0f,
+				shotGen.ballOnPath(new Vec2(2.980f, 2.980f), new Vec2(3.0f,
 						2.0f), new MockPoolBall(3.0f, 3.0f)));
 		// just outside
 		assertFalse("The path isn't clear(Just inside Ball angled)",
-				shotGen.ballNotOnPath(new Vec2(2.975f, 2.975f), new Vec2(3.0f,
+				shotGen.ballOnPath(new Vec2(2.975f, 2.975f), new Vec2(3.0f,
 						2.0f), new MockPoolBall(3.0f, 3.0f)));
 		// just outside
 
