a9ef129e3b7ddf9864dd8886f8da7c82582e2853
anthonyptetlow <anthonyptetlow@gmail.com>
2014-03-25 16:55:17 +0000

    Restroed Table Setup

--- src/model/PoolTable.java
+++ src/model/PoolTable.java
@@ -77,43 +77,41 @@ public class PoolTable extends Observable implements IPoolTable {
 		float startX = 2.025f;
 		float startY = 0.675f;
 		balls.add(new PoolBall(startX, startY, Color.RED, environment));
-		//
-		// balls.add(new PoolBall(startX + 0.054f, startY - 0.027f,
-		// Color.YELLOW,
-		// environment));
-		//
-		// balls.add(new PoolBall(startX + 0.054f, startY + 0.027f,
-		// Color.YELLOW,
-		// environment));
 
-		// balls.add(new PoolBall(startX + (2 * 0.054f), startY - (2 * 0.027f),
-		// Color.RED, environment));
+		balls.add(new PoolBall(startX + 0.054f, startY - 0.027f, Color.YELLOW,
+				environment));
+
+		balls.add(new PoolBall(startX + 0.054f, startY + 0.027f, Color.YELLOW,
+				environment));
+
+		balls.add(new PoolBall(startX + (2 * 0.054f), startY - (2 * 0.027f),
+				Color.RED, environment));
 		balls.add(new PoolBall(startX + (2 * 0.054f), startY, Color.BLACK,
 				environment));
-		// balls.add(new PoolBall(startX + (2 * 0.054f), startY + (2 * 0.027f),
-		// Color.RED, environment));
+		balls.add(new PoolBall(startX + (2 * 0.054f), startY + (2 * 0.027f),
+				Color.RED, environment));
 
-		// balls.add(new PoolBall(startX + (3 * 0.054f), startY - (3 * 0.027f),
-		// Color.YELLOW, environment));
-		// balls.add(new PoolBall(startX + (3 * 0.054f), startY - (1 * 0.027f),
-		// Color.YELLOW, environment));
-		// balls.add(new PoolBall(startX + (3 * 0.054f), startY + (1 * 0.027f),
-		// Color.RED, environment));
-		// balls.add(new PoolBall(startX + (3 * 0.054f), startY + (3 * 0.027f),
-		// Color.YELLOW, environment));
-		//
-		// balls.add(new PoolBall(startX + (4 * 0.054f), startY - (4 * 0.027f),
-		// Color.RED, environment));
-		//
-		// balls.add(new PoolBall(startX + (4 * 0.054f), startY - (2 * 0.027f),
-		// Color.YELLOW, environment));
-		// balls.add(new PoolBall(startX + (4 * 0.054f), startY, Color.RED,
-		// environment));
-		// balls.add(new PoolBall(startX + (4 * 0.054f), startY + (2 * 0.027f),
-		// Color.YELLOW, environment));
-		//
-		// balls.add(new PoolBall(startX + (4 * 0.054f), startY + (4 * 0.027f),
-		// Color.RED, environment));
+		balls.add(new PoolBall(startX + (3 * 0.054f), startY - (3 * 0.027f),
+				Color.YELLOW, environment));
+		balls.add(new PoolBall(startX + (3 * 0.054f), startY - (1 * 0.027f),
+				Color.YELLOW, environment));
+		balls.add(new PoolBall(startX + (3 * 0.054f), startY + (1 * 0.027f),
+				Color.RED, environment));
+		balls.add(new PoolBall(startX + (3 * 0.054f), startY + (3 * 0.027f),
+				Color.YELLOW, environment));
+
+		balls.add(new PoolBall(startX + (4 * 0.054f), startY - (4 * 0.027f),
+				Color.RED, environment));
+
+		balls.add(new PoolBall(startX + (4 * 0.054f), startY - (2 * 0.027f),
+				Color.YELLOW, environment));
+		balls.add(new PoolBall(startX + (4 * 0.054f), startY, Color.RED,
+				environment));
+		balls.add(new PoolBall(startX + (4 * 0.054f), startY + (2 * 0.027f),
+				Color.YELLOW, environment));
+
+		balls.add(new PoolBall(startX + (4 * 0.054f), startY + (4 * 0.027f),
+				Color.RED, environment));
 
 	}
 
