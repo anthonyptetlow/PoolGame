063ae23407aa7a26c204fc0bb5d2bce632153149
anthonyptetlow <anthonyptetlow@gmail.com>
2014-04-27 16:18:45 +0100

    Minor code cleanup

--- src/ai/imp/MaxShotSelector.java
+++ src/ai/imp/MaxShotSelector.java
@@ -10,7 +10,6 @@ public class MaxShotSelector implements IShotSelector {
 
 	@Override
 	public Vec2 selectShot(Map<Vec2, Integer> shotsAndScores) {
-		// TODO Maybe add a simulation to check?? then reset the table
 		Vec2 selectedShot = null;
 		Integer maxScore = 0;
 		for (Vec2 shot : shotsAndScores.keySet()) {
@@ -23,7 +22,6 @@ public class MaxShotSelector implements IShotSelector {
 				&& shotsAndScores.keySet().iterator().hasNext()) {
 			selectedShot = shotsAndScores.keySet().iterator().next();
 		} else {
-			// TODO Fix random shot if non available
 			selectedShot = new Vec2((float) Math.random(),
 					(float) Math.random());
 		}
